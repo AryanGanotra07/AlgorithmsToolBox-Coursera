be68f7b2a42f40e5f379ed28f852373d77ad6457
Aryan Ganotra <aryanganotra7@gmail.com>
2020-06-17 11:00:18 +0530

    Money change greedy

---
+++ MoneyChange.cpp
@@ -0,0 +1,21 @@
+#import <iostream>
+using namespace std;
+
+int total(int m) {
+    int d[] = {10, 5, 1};
+    int s = 0;
+    for (int i=0; i<3; ++i) {
+        s+=m/d[i];
+        m = m%d[i];
+    }
+    return s;
+
+    
+}
+
+int main() {
+    int m;
+    cin >> m;
+    cout << total(m) << '\n';
+    return 0;
+}
