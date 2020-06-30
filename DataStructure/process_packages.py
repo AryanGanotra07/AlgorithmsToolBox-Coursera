fc029ae715d5e7b101bf1807ceede8094c32d0c9
Aryan Ganotra <aryanganotra7@gmail.com>
2020-06-30 16:05:05 +0530

    complete processs package

--- DataStructure/process_packages.py
+++ DataStructure/process_packages.py
@@ -10,10 +10,41 @@ class Buffer:
     def __init__(self, size):
         self.size = size
         self.finish_time = []
+    
+    @property
+    def last_element(self):
+        return self.finish_time[-1]
+    
+    @property
+    def isEmpty(self):
+        if (len(self.finish_time) == 0):
+            return True
+        return False
+    
+    @property
+    def isFull(self):
+        if len(self.finish_time) == self.size:
+            return True
+        return False
+    
+    def flushProcessed(self, request):
+        while self.finish_time:
+            if self.finish_time[0] <= request.arrived_at:
+                self.finish_time.pop(0)
+            else:
+                break
+
 
     def process(self, request):
-        # write your code here
-        return Response(False, -1)
+        self.flushProcessed(request)
+        if self.isEmpty:
+            self.finish_time = [request.arrived_at + request.time_to_process]
+            return Response(False, request.arrived_at)
+        if self.isFull:
+            return Response(True, -1)
+        response = Response(False, self.last_element)
+        self.finish_time.append(request.time_to_process + self.last_element)
+        return response
 
 
 def process_requests(requests, buffer):
