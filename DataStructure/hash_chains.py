4ca51758daf5abcf7a3cfeab8273ee3b894f2de6
Aryan Ganotra <aryanganotra7@gmail.com>
2020-07-11 22:29:56 +0530

    hash chains complete

--- DataStructure/hash_chains.py
+++ DataStructure/hash_chains.py
@@ -1,14 +1,3 @@
-# python3
-
-class Query:
-
-    def __init__(self, query):
-        self.type = query[0]
-        if self.type == 'check':
-            self.ind = int(query[1])
-        else:
-            self.s = query[1]
-
 
 class QueryProcessor:
     _multiplier = 263
@@ -16,49 +5,70 @@ class QueryProcessor:
 
     def __init__(self, bucket_count):
         self.bucket_count = bucket_count
-        # store all strings in one list
-        self.elems = []
+        self.buckets = [[] for _ in range(bucket_count)]
 
     def _hash_func(self, s):
+       
         ans = 0
         for c in reversed(s):
             ans = (ans * self._multiplier + ord(c)) % self._prime
         return ans % self.bucket_count
 
-    def write_search_result(self, was_found):
-        print('yes' if was_found else 'no')
+    def add(self, string):
+      
+        hashed = self._hash_func(string)
+        bucket = self.buckets[hashed]
+        if string not in bucket:
+            self.buckets[hashed] = [string] + bucket
 
-    def write_chain(self, chain):
-        print(' '.join(chain))
+    def delete(self, string):
+        """Removes string from the table.
+        If there is no such string in the hash table,
+        then the query is ignored.
+        """
+        hashed = self._hash_func(string)
+        bucket = self.buckets[hashed]
+        for i in range(len(bucket)):
+            if bucket[i] == string:
+                bucket.pop(i)
+                break
 
-    def read_query(self):
-        return Query(input().split())
+    def find(self, string):
+        """Looks up for the string in the table.
+        Returns “yes” or “no” (without quotes) depending on whether
+        the table contains string or not.
+        """
+        hashed = self._hash_func(string)
+        if string in self.buckets[hashed]:
+            return "yes"
+        return "no"
 
-    def process_query(self, query):
-        if query.type == "check":
-            # use reverse order, because we append strings to the end
-            self.write_chain(cur for cur in reversed(self.elems)
-                        if self._hash_func(cur) == query.ind)
-        else:
-            try:
-                ind = self.elems.index(query.s)
-            except ValueError:
-                ind = -1
-            if query.type == 'find':
-                self.write_search_result(ind != -1)
-            elif query.type == 'add':
-                if ind == -1:
-                    self.elems.append(query.s)
-            else:
-                if ind != -1:
-                    self.elems.pop(ind)
+    def check(self, i):
+        """Returns the content of the i-th list in the table."""
+        return self.buckets[i]
 
-    def process_queries(self):
-        n = int(input())
-        for i in range(n):
-            self.process_query(self.read_query())
 
-if __name__ == '__main__':
+def process_queries(queries):
+    """Helper function which reads queries from standard input,
+    runs query processor and sends the results to standard output.
+    """
+    for query in queries:
+        command, arg = query.split()
+        if command == "add":
+            qp.add(arg)
+        elif command == "del":
+            qp.delete(arg)
+        elif command == "find":
+            print(qp.find(arg))
+        elif command == "check":
+            arg = int(arg)
+            print(" ".join(qp.check(arg)))
+
+
+if __name__ == "__main__":
     bucket_count = int(input())
-    proc = QueryProcessor(bucket_count)
-    proc.process_queries()
+    n = int(input())
+
+    qp = QueryProcessor(bucket_count)
+    queries = [input() for i in range(n)]
+    process_queries(queries)
